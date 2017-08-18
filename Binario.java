b675c264a091fb9bb365b4cff9be55b68758f07f
betolp100 <beto11_lp@hotmail.com>
2017-08-17 20:20:50 -0500

    Update Binario.java

--- Binario.java
+++ Binario.java
@@ -1,21 +1,42 @@
-
 package ed.mlárragap.a03;
 
 public class Binario
-{
- 
-    public static int fetch( int [] arreglo, int dato)
+{   
+    int dato;
+    int[] nuevVector;
+    
+    public int[] seleccion(int[] vector) 
+    {
+        int aux; 
+        for (int i = 0; i < vector.length - 1; i++) 
+        { 
+            int m = i; 
+            for (int j = i + 1; j < vector.length; j++) 
+            { 
+              if (vector[j] < vector [m]) 
+              { 
+                  m = j;
+              }
+            }
+            aux = vector[i];
+            vector[i]=vector[m];
+            vector[m]=aux;
+        }
+        return vector;
+    }
+    
+    public int fetch(int [] vector, int dato)
     {
         int inicio = 0;
-        int fin = arreglo.length - 1;
+        int fin = vector.length - 1;
         int centro;
         while (inicio <= fin)
         {
             centro = (inicio+fin) / 2;
-            if ( arreglo[centro] == dato )
+            if ( vector[centro] == dato )
             {
             return centro;
-            }else if ( arreglo[centro] < dato )
+            }else if ( vector[centro] < dato )
             {
                 inicio = centro+1;
             } else 
